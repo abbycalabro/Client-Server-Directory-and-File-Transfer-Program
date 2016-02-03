f20622edb23dc22d2a58a21f834c18b06148708e
Abby Meunier <abby.meunier@gmail.com>
2016-02-02 17:37:25 -0700

    Refactored code and wrote README

--- ftclient.py
+++ ftclient.py
@@ -1,123 +1,127 @@
 '''
-Program:		ftclient.py
-Assignment:     CS 372, Project 2
-Author:			Abby Meunier
-Email:			meuniera@oregonstate.edu
-Date:			11/29/2015
-Description:	Program takes two command line arguments: server hostname and port #.
-				Prompts user for a username handle and establishes a TCP connection with server.
-				Once connected, client and server exchange messages until either one enters 
-				the message "/quit", upon which the connection is closed and the program terminates.
-Resources used:
-				http://www.bogotobogo.com/python/python_network_programming_server_client_file_transfer.php
+Program:	    ftclient.py
+Author:		    Abby Meunier
+Email:		    meuniera@oregonstate.edu
+Date:		    11/29/2015
+Resources used:     http://www.bogotobogo.com/python/python_network_programming_server_client_file_transfer.php
+		    https://docs.python.org/2/tutorial/inputoutput.html
+		    http://stackoverflow.com/questions/120656/directory-listing-in-python
 '''
 
 from socket import *
 import sys
-import time
+import os
 
-def init_contact(serverName, serverPort):
-	#set up socket using validated arguments
-	clientSocket = socket(AF_INET, SOCK_STREAM)
+def init_contact(server_name, server_port):
+    #set up socket using validated arguments
+    client_socket = socket(AF_INET, SOCK_STREAM)
 
-	#connect to server
-	if clientSocket.connect((serverName, serverPort)) == -1:
-		print "Error connecting to", serverName, "port #", serverPort
-		sys.exit()
+    #connect to server
+    if client_socket.connect((server_name, server_port)) == -1:
+        print "Error connecting to", server_name, "port #", server_port
+        sys.exit()
 
-	return clientSocket
+    return client_socket
 
 def validate():
-	if len(sys.argv) < 5 or len(sys.argv) > 6:
-		print "Invalid number of arguments. See README.txt for more information."
-		sys.exit()
-	if sys.argv[3] != '-l' and sys.argv[3] != '-g':
-		print "Invalid <command> argument. See README.txt for more information."
-		sys.exit()
-	elif sys.argv[3] == '-l' and len(sys.argv) != 5:
-		print "Invalid number of arguments for command '-l'. See README.txt for more information."
-		sys.exit()
-	elif sys.argv[3] == '-g' and len(sys.argv) != 6:
-		print "Invalid number of arguments for command '-g'. See README.txt for more information."
-		sys.exit()
+    if len(sys.argv) < 5 or len(sys.argv) > 6:
+        print "Invalid number of arguments. See README.txt for more information."
+        sys.exit()
+    if sys.argv[3] != '-l' and sys.argv[3] != '-g':
+        print "Invalid <command> argument. See README.txt for more information."
+        sys.exit()
+    elif sys.argv[3] == '-l' and len(sys.argv) != 5:
+        print "Invalid number of arguments for command '-l'. See README.txt for more information."
+        sys.exit()
+    elif sys.argv[3] == '-g' and len(sys.argv) != 6:
+        print "Invalid number of arguments for command '-g'. See README.txt for more information."
+        sys.exit()
 
-def make_request(command, clientSocket):
-	#client quits or sends message
-	if command == "/quit":
-		print "Closing connection."
-		clientSocket.close()
-		print "Connection closed. Terminating program."
-		exit()
-	else:
-		clientSocket.send(command)
+def make_request(command, client_socket):
+    #client quits or sends message
+    if command == "/quit":
+        print "Closing connection."
+        client_socket.close()
+        print "Connection closed. Terminating program."
+        exit()
+    else:
+        client_socket.send(command)
 
-def receive_msg(clientSocket, serverName, serverPort):
-	#receive and print message, or end program
-	response = clientSocket.recv(500)
-	if len(response) == 0:
-		print "Server has quit. Closing connection."
-		clientSocket.close()
-		print "Connection closed. Now terminating."
-		sys.exit()
-	elif response == "SUCCESS":
-		return response
-	else:
-		print "%s:%d says: %s" % (serverName, serverPort, response)
-		return response
+def receive_msg(client_socket, server_name, server_port):
+    #receive and print message, or end program
+    response = client_socket.recv(1024)
+    if len(response) == 0 or response == "SUCCESS":
+        return "SUCCESS"
+    else:
+        print "%s:%d says: %s" % (server_name, server_port, response)
+        return response
+
+def receive_file(data_socket, file_name):
+    #check if duplicate fileName
+    for dirname, dirnames, filenames in os.walk('.'):
+        for cur_file in filenames:
+            if cur_file == file_name:
+                print "File '%s' already exists. Received file will be saved as 'dup_%s'" % (file_name, file_name)
+                file_name = "dup_" + file_name
+
+    #receive file data and write to new file
+    file = open(file_name, 'a')
+    data = data_socket.recv(1024)
+    while data:
+        with open(file_name, 'a') as f:
+            f.write(data)
+        data = data_socket.recv(1024)
+    print "File transfer complete"
+
+def list_directory(data_socket):
+    contents = []
+    data = data_socket.recv(500)
+
+    while data:
+        data = data_socket.recv(500)
+        print data	
 
 def main():
-	#validate command line arguments
-	#validate() 
-	
-	serverName = sys.argv[1]
-	serverPort = int(sys.argv[2])
-	command = sys.argv[3]
+    #validate command line arguments
+    validate() 
 
-	#request control connection to server
-	clientSocket = init_contact(serverName, serverPort)
-	
-	#send command and receive response to control connection
-	make_request(str(sys.argv[1:]), clientSocket)
-	control_response = receive_msg(clientSocket, serverName, serverPort)
+    server_name = sys.argv[1]
+    server_port = int(sys.argv[2])
+    command = sys.argv[3]
 
-	if control_response == "SUCCESS":
-		#create data socket
-		if command == '-l':
-			dataPort = int(sys.argv[4])
-			dataSocket = init_contact(serverName, dataPort)
-			#data_response = receive_msg(dataSocket, serverName, dataPort)
+    #request control connection to server
+    client_socket = init_contact(server_name, server_port)
 
-			print "Receiving directory structure from %s:%s" % (serverName, dataPort)
+    #send command and receive response to control connection
+    make_request(str(sys.argv[1:]), client_socket)
+    control_response = receive_msg(client_socket, server_name, server_port)
 
-			contents = []
+    if control_response == "SUCCESS":
+        if command == '-l':
+            data_port = int(sys.argv[4])
+            data_socket = init_contact(server_name, data_port)
+                
+            print "Receiving directory structure from %s:%s" % (server_name, data_port)
+            list_directory(data_socket)
+                
+        elif command == '-g':
+            data_port = int(sys.argv[5])
+            file_name = sys.argv[4]
+            data_socket = init_contact(server_name, data_port)
+            control_response = receive_msg(client_socket, server_name, server_port)
 
-			data = dataSocket.recv(1024)
-			while data:
-				contents.append(dataSocket.recv(1024))
-				data = dataSocket.recv(1024)
+            if control_response == "SUCCESS":
+                print "Receiving '%s' from %s:%s" % (file_name, server_name, data_port)
+                receive_file(data_socket, file_name)
 
-			for item in contents:
-				print item
+    else:
+        print "Error sending command. Terminating program."
+        sys.exit()
 
-
-			'''
-			while(dataSocket.recv(1024)):
-				directory_contents = dataSocket.recv(1024)
-				print "%s\n" % (directory_contents)
-			'''
-
-		elif command == '-g':
-			dataSocket = init_contact(serverName, int(sys.argv[5]))
-	else:
-		print "Error sending command. Terminating program."
-		sys.exit()
-
-
-	#close control connection
-	if clientSocket.close() == -1:
-		print "Error closing control connection."
-		sys.exit()
+    #close control connection
+    if client_socket.close() == -1:
+        print "Error closing control connection."
+        sys.exit()
 
 if __name__ == "__main__":
-	main()
-
+    main()
--- ftserver.cpp
+++ ftserver.cpp
@@ -1,20 +1,13 @@
-/****************************************************************************************
+/********************************************************************************************************************************
 Program:		ftserver.cpp
-Assignment:     CS 372, Project 2
 Author:			Abby Meunier
 Email:			meuniera@oregonstate.edu
 Date:			11/29/2015
-Description:	Program takes port # as a command line argument, creates
-				welcoming socket and begins listening for connection requests 
-				on the specified port. When a connection to client is established,
-				server sends messages and prints received messages to the 
-				terminal. If client or server enters the message "/quit", server
-				closes connection and listens for new connections until terminated
-				by a supervisor.
 Resources:
 				http://www.bogotobogo.com/cplusplus/sockets_server_client.php
                 http://www.tutorialspoint.com/unix_sockets/socket_server_example.html
-******************************************************************************************/
+				http://www.informit.com/guides/content.aspx?g=cplusplus&seqNum=245
+********************************************************************************************************************************/
 
 #include <iostream>
 #include <sys/socket.h>
@@ -25,12 +18,15 @@ Resources:
 #include <netdb.h>
 #include <stdio.h>
 #include <arpa/inet.h>
+#include <dirent.h>
+#include <vector>
 
 using namespace std;
 
 int start_up(int server_port) {
 	struct sockaddr_in server_addr;
 	int sockfd;
+	int opt_val = 1;
 
 	//create socket file descriptor
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -44,42 +40,119 @@ int start_up(int server_port) {
 	server_addr.sin_addr.s_addr = INADDR_ANY;
 	server_addr.sin_port = htons(server_port);
 	
+	//allow reuse of addresses
+	if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof(opt_val)) == -1) 
+		cout << "Error setting SO_REUSEADDR" << endl;
+
 	//bind to port
-	if(bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
+	if(bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
 		cout << "Error binding to host address." << endl;
+		exit(EXIT_FAILURE);
+	}
 
 	//start listening for connections
 	if(listen(sockfd, 5) < 0)
 		cout << "Error listening for connections." << endl;
-
+	
 	return sockfd;
 }
 
-int new_connection(int sockfd) {
+int new_connection(int sockfd, string type) {
 	int new_sockfd;
 	struct sockaddr_in client_addr;
 	socklen_t addr_size;
-	char host[1024];
-	char service[20];
-
-	//establish new control connection
+	char host[1024], service[20];
 	addr_size = sizeof(client_addr);
+
+	//establish new connection
 	new_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &addr_size);
+	
 	if(new_sockfd < 0)
 		cout << "Error accepting connection." << endl;
 	else {
 		getnameinfo((struct sockaddr *)&client_addr, sizeof(client_addr), host, sizeof(host), service, sizeof(service), 0);
-		cout << "Connection from " << host << " established." << endl;
+		cout << type << " connection from " << host << " established." << endl;
 	}
-
+	
 	return new_sockfd;
 }
 
-bool handle_request(int new_sockfd) {
-	int bytes_read, i;
+void send_directory(int sockfd) {
+	DIR *pdir;
+	struct dirent *current;
+	pdir = opendir(".");
+	string new_line = "\n";
+	string start = "start";
+
+	if(!pdir)
+		cout << "Error calling opendir()" << endl;
+	
+	if(send(sockfd, start.c_str(), strlen(start.c_str()), 0) < 0)
+		cout << "Error sending start to client" << endl;	
+
+	while((current = readdir(pdir))) {
+		//send all directory contents
+		cout << current->d_name << endl;
+		if(send(sockfd, current->d_name, strlen(current->d_name), 0) < 0)
+			cout << "Error sending " << current->d_name << " to client" << endl;	
+		if(send(sockfd, new_line.c_str(), strlen(new_line.c_str()), 0) < 0)
+			cout << "Error sending new_line to client" << endl;
+		sleep(0.2);	
+	}
+	
+	closedir(pdir);
+}
+
+void send_file(int data_sockfd, int control_sockfd, string file_name) {
+	DIR *pdir;
+	struct dirent *current;
+	bool file_exists = false;
+	pdir = opendir(".");
+	FILE *file_fd;
+	char buffer[2048];
+	string msg;
+
+	if(!pdir)
+		cout << "Error calling opendir()" << endl;
+	
+	//check if requested file exists in directory
+	while((current = readdir(pdir))) {
+		if(strcmp(current->d_name, file_name.c_str()) == 0) {
+			file_exists = true;
+			break;
+		}
+	}
+	closedir(pdir);
+
+	if(file_exists) {
+		//send to client
+		file_fd = fopen(file_name.c_str(), "r");
+		if(file_fd == NULL)
+			cout << "Error opening file" << endl;
+			
+		msg = "SUCCESS";
+		if(send(control_sockfd, msg.c_str(), strlen(msg.c_str()), 0) < 0)
+			cout << "Error sending SUCCESS" << endl;
+
+		while(fgets(buffer, sizeof(buffer), file_fd) != NULL) {
+			if(send(data_sockfd, buffer, strlen(buffer), 0) < 0)
+				cout << "Error sending file data" << endl;
+			sleep(0.01);
+		}
+		fclose(file_fd);
+	}
+	else {
+		cout << "File not found. Sending error message to client." << endl;
+		msg = "FILE NOT FOUND";
+		if(send(control_sockfd, msg.c_str(), strlen(msg.c_str()), 0) < 0)
+			cout << "Error sending error message to client" << endl;		
+	}
+}
+
+bool handle_request(int new_sockfd, int sockfd) {
+	int bytes_read, i, data_sockfd, welcome_sockfd, data_port;
 	char buffer[504];
-	char *tok;
-	char *args[504];
+	char *tok, *args[504];
 	const char *msg;	
 
 	//receive command
@@ -94,28 +167,60 @@ bool handle_request(int new_sockfd) {
 		exit(EXIT_FAILURE);
 	}
 	else if(bytes_read == 0) {
-		//close connection and exit
-		if(close(new_sockfd) !=  0)
-			cout << "Error closing connection" << endl;
-		else
-			cout << "Connection closed." << endl;
-		exit(EXIT_FAILURE);
+		return false;
 	}
 	
 	//parse buffer for command arguments
 	tok = strtok(buffer, "[',]\n ");
 	for(i = 0; tok != NULL; i++) {
 		args[i] = tok;
-		cout << "Current tok: " << args[i] << endl;
 		tok = strtok(NULL, "[',]\n ");
 	}
 
-	//handle request based on command
+	//list directory or send requested file
 	if(strcmp(args[2], "-l") == 0) {
-		cout << "Let me list that for for you!" << endl;
+		data_port = atoi(args[3]);
+		welcome_sockfd = start_up(data_port);				
+		
+		cout << "List directory requested on port " << data_port << endl;		
+		
+		if(welcome_sockfd < 1)
+			cout << "Error creating welcome_sockfd" << endl;
+
+		//tell client that server is ready for data connection	
+		msg = "SUCCESS";
+		if(send(new_sockfd, msg, strlen(msg), 0) < 0)
+			cout << "Error sending data connection message to client" << endl;
+		
+		data_sockfd = new_connection(welcome_sockfd, "Data");
+		if(data_sockfd < 1)
+			cout << "Error creating data connection" << endl;
+
+		//send directory to client	
+		cout << "Sending directory contents to port " << data_port << endl; 
+		send_directory(data_sockfd);
 	}	
 	else if(strcmp(args[2], "-g") == 0) {
-		cout << "Let me get that for for you!" << endl;
+		data_port = atoi(args[4]);
+		welcome_sockfd = start_up(data_port);				
+		
+		cout << "File '" << args[3] << "' requested on port " << args[4] << endl;
+		
+		if(welcome_sockfd < 1)
+			cout << "Error creating welcome_sockfd" << endl;
+
+		//tell client that server is ready for data connection	
+		msg = "SUCCESS";
+		if(send(new_sockfd, msg, strlen(msg), 0) < 0)
+			cout << "Error sending data connection message to client" << endl;
+		
+		data_sockfd = new_connection(welcome_sockfd, "Data");
+		if(data_sockfd < 1)
+			cout << "Error creating data connection" << endl;
+		
+		//send requested file to client
+		cout << "Sending '" << args[3] << "' to port " << args[4] << endl;
+		send_file(data_sockfd, new_sockfd, args[3]);
 	}
 	else {
 		//send error message to client
@@ -123,27 +228,35 @@ bool handle_request(int new_sockfd) {
 		if(send(new_sockfd, msg, strlen(msg), 0) < 0)
 			cout << "Error sending error message to client" << endl;
 	}
+
+	//after request has been handled, close data socket
+	if(close(data_sockfd) == -1 || close(welcome_sockfd) == -1)
+		cout << "Error closing data socket" << endl;
+
 	return true;
 }
 
+
 int main(int argc, char* argv[]) {
-	int sockfd, new_sockfd;
+	int sockfd, control_sockfd;
 	int server_port = atoi(argv[1]);
 
-	//set up sockets and start listening
+	//set up socket and start listening
 	sockfd = start_up(server_port);
 
-	cout << "Server open on " << server_port << endl;
-	
-	//accept new control connection 
-	new_sockfd = new_connection(sockfd);
+	//listen on server_port until program is terminated 
+	while(1) {	
+		cout << "Server open on " << server_port << endl;
+		
+		//establish control connection 
+		control_sockfd = new_connection(sockfd, "Control");
 
-	if(!handle_request(new_sockfd))
-		cout << "Error handling client request" << endl;
-
-	//close data connection
-	if(close(new_sockfd) == -1) 
-		cout << "Error closing data socket." << endl;
+		while(1) {
+			//handle client request
+			if(!handle_request(control_sockfd, sockfd)) 
+				break;
+		}
+	}
 	
 	return 0;
 }
